b43e6c5d7067e2c7b6a78c3c10a249d197824d0d
daniel watts <danielw@.staff.7d>
2014-10-08 16:12:49 +0100

    remove pointless class

---
+++ http-requests/http-requests/HttpRequestsTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace http_requests
+{
+	[TestFixture]
+    public class HttpRequestsTests
+    {
+		[Test]
+		public void Should_return_some_release_metadata()
+		{
+			
+		}
+    }
+}
