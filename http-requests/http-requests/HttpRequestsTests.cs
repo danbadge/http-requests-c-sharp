302f18d654dc8e16948c12514f65a2c454a39d5b
daniel watts <danielw@.staff.7d>
2014-10-08 16:26:18 +0100

    got the old c# web request working

--- http-requests/http-requests/HttpRequestsTests.cs
+++ http-requests/http-requests/HttpRequestsTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
 
 namespace http_requests
 {
@@ -6,9 +9,27 @@ namespace http_requests
     public class HttpRequestsTests
     {
 		[Test]
-		public void Should_return_some_release_metadata()
+		public void Should_return_a_release()
 		{
-			
+			const string url = "http://catalogue-metadata-http-cache.prod.svc.7d/releases/1012124";
+
+			var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+			httpRequest.Method = "GET";
+
+			Console.WriteLine(httpRequest.Method + " " + url);
+
+			var response = httpRequest.GetResponse();
+
+			string body;
+			using (var stream = response.GetResponseStream())
+			{
+				body = new StreamReader(stream).ReadToEnd();
+			}
+
+			Console.WriteLine(body);
+
+			Assert.That(body, Is.StringContaining("\"id\":1012124"));
+			Assert.That(body, Is.StringContaining("\"title\":\"Bon Jovi Greatest Hits\""));
 		}
     }
 }
