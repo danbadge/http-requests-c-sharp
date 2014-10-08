da3b4df9ef5ed25deb31a613fd0ce6b8e9fbd0f2
daniel watts <danielw@.staff.7d>
2014-10-08 11:41:27 +0100

    new empty solution

---
+++ http-requests/http-requests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_requests
+{
+    public class Class1
+    {
+    }
+}
